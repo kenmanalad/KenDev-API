5c0e7b5e3f668b560fd45c1d81271aeaa3c7bbf0
kenmanalad <76421176+kenmanalad@users.noreply.github.com>
2024-08-02 18:07:07 +0800

    UPDATE: Added Google Authentication Feature

---
+++ routes/authentication/google/google-auth-request.js
@@ -0,0 +1,50 @@
+import express from "express";
+import { config } from "dotenv";
+import { OAuth2Client } from "google-auth-library";
+
+config();
+
+const router = express.Router();
+
+router.use(( req, res, done) => {
+    res.header("Access-Control-Allow-Origin",'http://localhost:5173');
+
+    res.header("Referrer-Policy","no-referrer-when-downgrade");
+
+    done();
+});
+
+router.post("/google-request",async(req, res) => {
+
+    const redirectUrl = process.env.GOOGLE_REDIRECT_URL;
+
+    try{
+        const oAuth2Client = new OAuth2Client(
+            process.env.CLIENT_ID,
+            process.env.CLIENT_SECRET,
+            redirectUrl
+        );
+    
+        const authorizeURL = oAuth2Client.generateAuthUrl(
+            {
+                access_type: "offline",
+                scope: [ 
+                    process.env.GOOGLE_SCOPES 
+                ],
+                prompt: "consent"
+            }
+        );
+
+        res.json(
+            {
+                url:authorizeURL
+            }
+        );
+
+    } catch(error) {
+        console.error("Error occured while requesting auth url",error);
+    }
+
+});
+
+export default router;
