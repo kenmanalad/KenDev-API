9de127a7db2ab56b2ff1cf4bd87caa32f33daa2d
kenmanalad <76421176+kenmanalad@users.noreply.github.com>
2024-08-16 09:09:58 +0800

    UPDATE : ADDED PROFILE PIC UPLOAD

--- routes/profile/profile.js
+++ routes/profile/profile.js
@@ -1,15 +1,36 @@
 import express from "express"
 import passport from "passport";
-import Profile from "../../models/profile/profile.js";
-import User from "../../models/user.js";
+import multer from "multer";
 import registerProfile from "../../controllers/profile/register-profile.js";
+import { config } from "dotenv";
+
+
+config();
 const router = express.Router();
+
+//This is only temporary
+//Files will be saved in S3 buckets after Sprint 20
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+
+        cb(null,process.env.TEMP_URL_FOR_UPLOAD);
+    },
+    filename: (req, file, cb) => {
+
+        let newProfilePicPath = `${Date.now()}${file.originalname}`
+        cb(null,newProfilePicPath);
+        
+    }
+});
+
+const upload = multer({storage});
 
 router.post("/profile-registration",
     passport.authenticate(
         "jwt",
         {session:false}
-    )
+    ),
+    upload.single('profilePic')
     ,registerProfile
 );
 
